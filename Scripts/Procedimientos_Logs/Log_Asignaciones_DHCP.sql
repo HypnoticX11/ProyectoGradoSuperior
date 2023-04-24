8d451695788b710f8d66520ca1ff2056e6820406
Diego <dprieto600@gmail.com>
2023-04-24 11:17:48 +0200

    Log_Asignaciones_DHCP

---
+++ Scripts/Procedimientos_Logs/Log_Asignaciones_DHCP.sql
@@ -0,0 +1,20 @@
+DROP PROCEDURE IF EXISTS Log_Asignaciones_DHCP;
+-- Cambiamos el delimitador por "$$" 
+DELIMITER $$
+-- Creamos un procedimiento al que le pasamos un parámetro float
+CREATE PROCEDURE Borrado_Tablas()
+
+-- Iniciamos el procedimiento
+BEGIN
+    -- Declaramos las variables Que necesitamos
+    Declare Ruta varchar(100);
+
+    SET @RUTA = concat("SELECT * FROM IP_Asignadas INTO OUTFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads",DATE_FORMAT(NOW(),'_%Y_%m_%d_%H_%i_%s'), '.txt ',
+                        "' FIELDS TERMINATED BY ','");
+    PREPARE statement FROM @RUTA;
+    EXECUTE statement;
+    TRUNCATE TABLE IP_Asignadas;
+
+END$$
+
+delimiter ;
