ac222cb07d4eaf5e318d0d08e43aaa05467abd33
Diego <dprieto600@gmail.com>
2023-04-25 13:10:29 +0200

    arreglos

--- Scripts/Procedimientos_Logs/Log_Impresion.sql
+++ Scripts/Procedimientos_Logs/Log_Impresion.sql
@@ -8,17 +8,17 @@ BEGIN
     -- Declaramos las variables Que necesitamos
     Declare Ruta varchar(100);
 
-    SET @RUTA = concat("SELECT * FROM Drivers_Impresoras INTO OUTFILE '/otp/lampp/mysql/logs_Servicios/Impresión/Log_Drivers_Impresion",DATE_FORMAT(NOW(),'_%Y_%m_%d_%H_%i_%s'), '.txt ', "' FIELDS TERMINATED BY ','");
+    SET @RUTA = concat("SELECT * FROM Drivers_Impresoras INTO OUTFILE '/opt/lampp/mysql/logs_Servicios/Impresion/Log_Drivers_Impresion",DATE_FORMAT(NOW(),'_%Y_%m_%d_%H_%i_%s'), '.txt ', "' FIELDS TERMINATED BY ','");
     PREPARE statement FROM @RUTA;
     EXECUTE statement;
-    TRUNCATE TABLE Drivers_Impresorasn;
+    TRUNCATE TABLE Drivers_Impresoras;
 
-    SET @RUTA = concat("SELECT * FROM Puertos_Impresoras INTO OUTFILE '/otp/lampp/mysql/logs_Servicios/Impresión/Log_Puertos_Impresion",DATE_FORMAT(NOW(),'_%Y_%m_%d_%H_%i_%s'), '.txt ', "' FIELDS TERMINATED BY ','");
+    SET @RUTA = concat("SELECT * FROM Puertos_Impresoras INTO OUTFILE '/opt/lampp/mysql/logs_Servicios/Impresion/Log_Puertos_Impresion",DATE_FORMAT(NOW(),'_%Y_%m_%d_%H_%i_%s'), '.txt ', "' FIELDS TERMINATED BY ','");
     PREPARE statement FROM @RUTA;
     EXECUTE statement;
     TRUNCATE TABLE Puertos_Impresoras;
 
-    SET @RUTA = concat("SELECT * FROM Impresoras INTO OUTFILE '/otp/lampp/mysql/logs_Servicios/Impresión/Log_Impresoras",DATE_FORMAT(NOW(),'_%Y_%m_%d_%H_%i_%s'), '.txt ', "' FIELDS TERMINATED BY ','");
+    SET @RUTA = concat("SELECT * FROM Impresoras INTO OUTFILE '/opt/lampp/mysql/logs_Servicios/Impresion/Log_Impresoras",DATE_FORMAT(NOW(),'_%Y_%m_%d_%H_%i_%s'), '.txt ', "' FIELDS TERMINATED BY ','");
     PREPARE statement FROM @RUTA;
     EXECUTE statement;
     TRUNCATE TABLE Impresoras;
--- Scripts/Procedimientos_Logs/Log_Jobs_Impresion.sql
+++ Scripts/Procedimientos_Logs/Log_Jobs_Impresion.sql
@@ -9,7 +9,7 @@ BEGIN
     -- Declaramos las variables Que necesitamos
     Declare Ruta varchar(100);
 
-    SET @RUTA = concat("SELECT * FROM Cola_Impresión INTO OUTFILE '/otp/lampp/mysql/logs_Servicios/Impresión/Log_jobs_Impresion",DATE_FORMAT(NOW(),'_%Y_%m_%d_%H_%i_%s'), '.txt ', "' FIELDS TERMINATED BY ','");
+    SET @RUTA = concat("SELECT * FROM Cola_Impresión INTO OUTFILE '/opt/lampp/mysql/logs_Servicios/Impresion/Log_jobs_Impresion",DATE_FORMAT(NOW(),'_%Y_%m_%d_%H_%i_%s'), '.txt ', "' FIELDS TERMINATED BY ','");
     PREPARE statement FROM @RUTA;
     EXECUTE statement;
     TRUNCATE TABLE Cola_Impresión ;
