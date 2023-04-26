50c04f6c1a30eb3aaf6fe0e61f5dbf4afbfc3537
Diego <dprieto600@gmail.com>
2023-04-26 19:53:17 +0200

    web

--- Web/Principal.php
+++ Web/Principal.php
@@ -7,24 +7,38 @@
   </head>
 
   <body>
-    <h1>Sistema de Monitorización de Equipos y servicios</h1> <br><br>
-
-    <div style="display: flex; width: 300px; height: 300px; margin-left: 35%;">
-      <img src="Servidor.png" style="flex: 1; margin-right: 165px;">
-      <img src="Servidor.png" style="flex: 1;">
-    </div>
-
-    
-
-    <br><br><br><br><br><br><br><br><br><br><br>
-    <div class="barra">
-      <div class="relleno"></div>
-    </div>
-
     <?php
       // Establecer conexión
       $conn = mysqli_connect("192.168.1.4", "Proyecto", "Admin123.", "INFO_EQUIPOS");
+    ?>
 
+    <h1>Sistema de Monitorización de Equipos y servicios</h1> <br><br>
+
+    <p>Windows Server 1</p>
+    <div style="display: flex; width: 300px; height: 300px; margin-left: 35%;">
+      <img src="Servidor.png" style="flex: 1; margin-right: 165px;">
+      <img src="Servidor.png" style="flex: 1;">
+    </div><br><br>
+
+    <?php
+      $sql = "SELECT * FROM Datos_Equipos Where IP='192.168.1.2'";
+      $resultado = mysqli_query($conn, $sql);
+
+      $resultado = mysqli_fetch_assoc($resultado)
+
+      $Ram_total = $resultado["RAM_Total"];
+      $Ram_USO = $resultado["RAM_Uso"];
+      $Ram_total = str_replace(',', '.', $Ram_total );
+      $Ram_USO = str_replace(',', '.', $Ram_USO );
+      $porcentaje = floatval(($Ram_USO*100)/$Ram_total);
+      echo $porcentaje;
+    ?>
+    
+    <div class="barra">
+      <div class="relleno" style="width: <?php echo $porcentaje; ?>%;"></div>
+    </div>
+
+    <?php
       // Ejecutar consulta
       $sql = "SELECT * FROM Datos_Equipos";
       $resultado = mysqli_query($conn, $sql);
@@ -32,7 +46,7 @@
       // Mostrar resultados
       while ($fila = mysqli_fetch_assoc($resultado)) {
         echo "IP: " . $fila["IP"] . "<br>";
-        echo "Uso_CPU: " . $fila["Espacio_Usado_Disco"] . "<br>";
+        echo "Uso_CPU: " . $fila["Espacio_Usado_Disco"] . "%<br>";
         echo "<br>";
       }
 
